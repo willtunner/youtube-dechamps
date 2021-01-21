f8b4f2fb91bc456ada57eb493585a1a88a1910d0
William <williampereira21@gmail.com>
2021-01-21 09:29:38 -0300

    adicionado api do convertkit

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,8 +1,22 @@
-function tempo(req, res) {
+async function tempo(req, res) {
     const dynamicDate = new Date();
 
+    const apiSecret = process.env.CONVERTKIT_API_SECRET;
+
+    // ? Pega os dados da url da api
+    const subscribersResponse = await fetch(`https://api.convertkit.com/v3/subscribers?api_secret=${apiSecret}&from=2016-02-01&to=2015-02-28`);
+    // ? Transforma em json a resposta acima
+    const subscribersResponseJson = await subscribersResponse.json();
+    // ? Pega um campo especifico 'total_subscribers'
+    const inscritos = subscribersResponseJson.total_subscribers;
+
+
+
+
+
     res.json({
-        date: dynamicDate.toGMTString()
+        date: dynamicDate.toGMTString(),
+        inscritos: inscritos
     })
 }
 export default tempo;
